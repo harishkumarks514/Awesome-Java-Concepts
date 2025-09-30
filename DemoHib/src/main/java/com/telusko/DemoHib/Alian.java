0fd4a08bd9b4444f1b5cfb33e9c190dcd96f009d
harishkumarks514 <44613619+harishkumarks514@users.noreply.github.com>
2025-09-30 22:11:25 +0530

    Add files via upload

---
+++ DemoHib/src/main/java/com/telusko/DemoHib/Alian.java
@@ -0,0 +1,30 @@
+package com.telusko.DemoHib;
+
+public class Alian  // POJO
+{
+
+	private int aid;
+	private String name;
+	private String color;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+	
+}
---
+++ DemoHib/src/main/java/com/telusko/DemoHib/App.java
@@ -0,0 +1,21 @@
+package com.telusko.DemoHib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	Alian telusko = new Alian();
+    	telusko.setAid(101);
+    	telusko.setColor("Green");
+    	telusko.setName("Harish");
+         
+    	 
+    	
+    	 
+    }
+}
