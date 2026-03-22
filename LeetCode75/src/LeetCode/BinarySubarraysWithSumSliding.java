baa92389dade89182d37c4fa3f985ef2fa8a1695
Harish Kumar K S <44613619+harishkumarks514@users.noreply.github.com>
2026-03-22 21:42:00 +0530

    Add files via upload

---
+++ LeetCode75/src/LeetCode/BinarySubarraysWithSumSliding.java
@@ -0,0 +1,34 @@
+package LeetCode;
+
+public class BinarySubarraysWithSumSliding {
+
+    public static int numSubarraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+
+    private static int atMost(int[] nums, int goal) {
+        if (goal < 0) return 0;
+
+        int left = 0, sum = 0, count = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+
+            while (sum > goal) {
+                sum -= nums[left++];
+            }
+
+            count += right - left + 1;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, 1, 0, 1};
+        int goal = 2;
+
+        int result = numSubarraysWithSum(nums, goal);
+        System.out.println("Number of subarrays: " + result);
+    }
+}
